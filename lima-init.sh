5fefa4467c96af3de9452897213cccef8130870a
seagren.tim <timseagren@2MFZX33-SEAGREN.localdomain>
2022-02-12 17:11:59 -0700

    working on debian installation

--- lima-init.sh
+++ lima-init.sh
@@ -13,9 +13,28 @@ EOF
 limafile=${1:-"default.yaml"}
 
 debian_install(){
-    LIMA_VERSION=${LIMA_VERSION:-"v0.8.2"}
+  declare -a qemu_deps=(qemu qemu-system qemu-utils)
+  for dep in "${qemu_deps[@]}"; do
+    if ! dpkg -l | grep -q "\b$dep\b" 2>&1 >/dev/null; then
+      sudo apt-get install $dep
+    fi
+  done
+  # if ! command -v lima 2>&1 >/dev/null ; then
+  #   LIMA_VERSION=${LIMA_VERSION:-"0.8.2"}
+  #   ARCH="$(uname -m)"
+  #   echo "Downloading lima $LIMA_VERSION"
+    # curl -fsSL  https://github.com/lima-vm/lima/releases/download/v${LIMA_VERSION}/lima-${LIMA_VERSION}-Linux-${ARCH}.tar.gz | tar Cxzvm /usr/local
+  # fi
+  if ! command -v lima 2>&1 >/dev/null ; then
+    LIMA_VERSION=${LIMA_VERSION:-"0.8.2"}
     ARCH="$(uname -m)"
-    curl -o lima.tar.gz https://github.com/lima-vm/lima/releases/download/${LIMA_VERSION}/lima-${LIMA_VERSION}-Linux-${ARCH}.tar.gz
+    echo "Downloading lima $LIMA_VERSION"
+    curl -fsSL https://github.com/lima-vm/lima/archive/refs/tags/v0.8.2.tar.gz | tar xzvf -
+    cd lima-${LIMA_VERSION}
+    make && sudo make install
+    cd -
+    rm -rf lima-${LIMA_VERSION}
+  fi
     #TODO Ubuntu Docker CLI installation
     #TODO Ubuntu Docker Compose installation
 }
